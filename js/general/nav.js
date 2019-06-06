3f03398dab144446224accb1cccdd78db4a284d0
CHENGCHANGHU <1669675499@qq.com>
2019-06-06 10:42:57 +0800

    logo change

--- js/general/nav.js
+++ js/general/nav.js
@@ -1,10 +1,10 @@
 $(function () {
-    console.log($(".top-title-box").eq(0).outerHeight());
-    console.log($(".logo-box").eq(0).outerHeight());
-    console.log(($(".top-title-box").eq(0).outerHeight() - $(".logo-box").eq(0).outerHeight()) / 2);
-    $(".logo-box").eq(0).css('margin-top',
-        (Math.ceil($(".top-title-box").eq(0).outerHeight() - $(".logo-box").eq(0).outerHeight()) / 2) + 'px');
+    console.log('.top-title-box:' + $(".top-title-box").eq(0).outerHeight());
+    console.log('logo-box img:' + $(".logo-box img").eq(0).outerHeight());
+    console.log(($(".top-title-box").eq(0).outerHeight() - $(".logo-box img").eq(0).outerHeight()) / 2);
+    // $(".logo-box img").eq(0).css('margin-top',
+    //     (Math.ceil($(".top-title-box").eq(0).outerHeight() - $(".logo-box img").eq(0).outerHeight()) / 2) + 'px');
 
-    $(".bottom-link-box ul li a").css("font-size", "10pt");
+    // $(".bottom-link-box ul li a").css("font-size", "10pt");
 
 });
